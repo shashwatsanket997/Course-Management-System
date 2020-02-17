03cd1b3126c85c25dd69f4de2774b10373bbf1bb
Shashwat Sanket <shashwatsanket997@gmail.com>
2020-02-17 13:45:59 +0530

    rem: Token Authentication, Switch to session-cookie authentication

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const cnst = require('./const');
 const router = require('./routes/routes');
 const app = express();
 
-
-// A simple Token-Based Authentication implemented using Vanila Javascript
-// All the authentication required urls defined in const.js will
-// require a token (16 chars) in their request body
-// Token will be allocated upon login, or register followed by internal login
-// On Logout the token will be deallocated or removed from the store
-// A storage for Session Management basically a hash map with {token : userId}
-let sessionStore = {};
 
 // Building and Configuring the server
 
@@ -22,6 +15,18 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json());
 
+// Setting session 
+app.use(session({
+    secret: 'NeverStopLearning',
+    name: 'cms',
+    cookie: {
+        maxAge: cnst.SESSION_TIME,
+        secure: false,
+    },
+    resave: false,
+    saveUninitialized: true,
+}))
+
 //Exposing the static files
 app.use(express.static(__dirname + '/public'));
 
@@ -31,8 +36,6 @@ app.set('views', './views');
 
 //Auth Token Verification Middleware: For A custom Token allocation and session management
 app.use((req, res, next) => {
-    let token = req.body.token;
-    console.log(sessionStore);
     //Rremove trailing '/' if present
     let url = req.url.replace(/\/$/, "");
     //if the url is public, that is no auth required--> simply pass
@@ -40,36 +43,13 @@ app.use((req, res, next) => {
         return next();
     }
     // For auth required urls
-    //If token is undefined,null,emptyString
-    if (!token) {
-        // For JSON response
-        if (url.includes('/api')) {
-            return res.status(401).json({
-                error: "Authentication Token required. Please login..."
-            });
-        } else {
-            return res.redirect('/');
-        }
-    } else {
-        //Token is provided
-        // Checking the validity of auth token
-        if (!(token in sessionStore)) {
-            if (url.includes('/api')) {
-                return res.status(400).json({
-                    error: "Invalid Authentication Token provided"
-                })
-            } else {
-                return res.redirect('/', { 'error': "Session Expired... Please relogin" })
-            }
-        } else {
-            //Token Present in the Session Store --> Good To Go
-            return next();
-        }
+    // Checking if the session for the user exists or not
+    if (!req.session.user) {
+        return res.redirect("login");
     }
+    // Session exists: Good to go
+    return next();
 })
-
-// Making the sessionStore object availble globally 
-global.sessionStore = sessionStore;
 
 //Routing the requests
 router(app);
