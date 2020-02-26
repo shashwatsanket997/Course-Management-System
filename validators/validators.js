221fde79ad9bf75a9b658e4f8fb79e80c05b9d52
Shashwat Sanket <34893157+shashwatsanket997@users.noreply.github.com>
2020-02-26 22:57:14 +0530

    fix(validator.js) : A minor spell fix

--- validators/validators.js
+++ validators/validators.js
@@ -5,12 +5,12 @@ const UserType = require('../const').UserType;
 
 module.exports.login = [
     check('username', 'Username is required').not().isEmpty(),
-    check('username', 'Password is required').not().isEmpty()
+    check('password', 'Password is required').not().isEmpty()
 ]
 
 module.exports.register = [
     check('username', 'Username is required').not().isEmpty(),
-    check('username', 'Password is required').not().isEmpty(),
+    check('password', 'Password is required').not().isEmpty(),
     check('username', 'Username should be greater than 3 chars').isLength({ min: 4 }),
     check('password', 'password should be greater than 4 chars').isLength({ min: 4 }),
     check('name', 'Name is required').not().isEmpty(),
